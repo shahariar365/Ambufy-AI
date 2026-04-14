cfc2cddb328cf6272a39b61cf4db6e0b13830fb0
shahariar365 <shahariarr365@gmail.com>
2026-04-14 16:34:47 +0600

    Final frontend setup for hosting

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xwiauzikjiorooslsiwo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh3aWF1emlramlvcm9vc2xzaXdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MTIzODgsImV4cCI6MjA5MTI4ODM4OH0.JCIGLAeWSHpIxld6y8gLYQphFB0jWkFK02iEeden2qc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
